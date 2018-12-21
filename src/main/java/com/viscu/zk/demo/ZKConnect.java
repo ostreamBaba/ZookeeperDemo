db53f68eaecec72786e859de73a9e1b489c26791
ostreamBaba <1160832305@qq.com>
2018-12-21 20:05:39 +0800

    建立客户端与zk服务器的连接

---
+++ src/main/java/com/viscu/zk/demo/ZKConnect.java
@@ -0,0 +1,45 @@
+package com.viscu.zk.demo;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * @ Create by ostreamBaba on 18-12-21
+ * @ zookeeper 连接Demo演示
+ */
+
+public class ZKConnect implements Watcher{
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(ZKConnect.class);
+
+    //单机版本
+    //public static final String zkServerPath = "127.0.0.1:2281";
+
+    //集群版本
+    public static final String zkServerPath = "127.0.0.1:2281,127.0.0.1:2282,127.0.0.1:2283";
+
+
+    public static final Integer timeout = 5000;
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        ZooKeeper zk = new ZooKeeper(zkServerPath, timeout, new ZKConnect());
+        //服务端地址 超时时间 所触发的watcher事件
+
+        LOGGER.debug("客户端开始连接zookeeper服务器...");
+        LOGGER.debug("连接状态： {}", zk.getState()); //连接中
+        Thread.sleep(2000); //设置暂停 zk连接需要一定时间
+        LOGGER.debug("连接状态： {}", zk.getState()); //已经连接
+    }
+
+
+    //watcher事件有多个 针对父节点的增删改事件 针对子节点的增删事件(NodeChanged) 改不触发watcher事件
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        LOGGER.debug("接收到watch通知: {}", watchedEvent);
+    }
+}
